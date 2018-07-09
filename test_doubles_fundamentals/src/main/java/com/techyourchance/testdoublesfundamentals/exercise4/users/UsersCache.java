6bd85f417dd1cb0baf328f5482022b4b5a1b722a
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-07-09 19:15:21 +0300

    Adding nullable annotation to UsersCache in exercise 4

--- test_doubles_fundamentals/src/main/java/com/techyourchance/testdoublesfundamentals/exercise4/users/UsersCache.java
+++ test_doubles_fundamentals/src/main/java/com/techyourchance/testdoublesfundamentals/exercise4/users/UsersCache.java
@@ -1,9 +1,11 @@
 package com.techyourchance.testdoublesfundamentals.exercise4.users;
 
+import org.jetbrains.annotations.Nullable;
+
 public interface UsersCache {
 
     void cacheUser(User user);
 
-    User getUser(String userId);
+    @Nullable User getUser(String userId);
 
 }
