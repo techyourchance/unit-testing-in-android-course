55250f9f2441256c7cf6e1cfe440d2e75f8abc6e
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2018-07-09 15:42:34 +0300

    Fixing uninteded additional bug in exercise 2

--- unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicator.java
+++ unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise2/StringDuplicator.java
@@ -6,12 +6,8 @@ public class StringDuplicator {
      * @return concatenation of the argument with itself e.g. "hi" -> "hihi"
      */
     public String duplicate(String string) {
-        if (string.length() <= 1) {
-            return string;
-        } else {
-            // the bug is triplication instead of duplication
-            return string + string + string;
-        }
+        // the bug is triplication instead of duplication
+        return string + string + string;
     }
 
 }
