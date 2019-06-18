b3e26496846ea547503231cd9224f05a4d6c46ae
Vasiliy Zukanov <techyourchance@gmail.com>
2019-06-18 11:42:34 +0300

    Solution to Exercise 9. NOTE: I, personally, don't think that this solution uses the optimal implementation of test doubles. Just wanted to show you alternative approach.

--- tutorial_android_application/src/main/java/com/techyourchance/unittesting/questions/QuestionDetails.java
+++ tutorial_android_application/src/main/java/com/techyourchance/unittesting/questions/QuestionDetails.java
@@ -1,5 +1,7 @@
 package com.techyourchance.unittesting.questions;
 
+import java.util.Objects;
+
 public class QuestionDetails {
 
     private final String mId;
@@ -24,5 +26,20 @@ public class QuestionDetails {
 
     public String getBody() {
         return mBody;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        QuestionDetails that = (QuestionDetails) o;
+        return Objects.equals(mId, that.mId) &&
+                Objects.equals(mTitle, that.mTitle) &&
+                Objects.equals(mBody, that.mBody);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(mId, mTitle, mBody);
     }
 }
