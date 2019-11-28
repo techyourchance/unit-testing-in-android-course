0fae38b95cc92764ffc9d33d22db639506dc4b1b
kirjava113 <annaluizalyjak@gmail.com>
2019-11-29 00:06:28 +0100

    exercise 1

--- unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidator.java
+++ unit_testing_fundamentals/src/main/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidator.java
@@ -3,8 +3,7 @@ package com.techyourchance.unittestingfundamentals.exercise1;
 public class NegativeNumberValidator {
 
     public boolean isNegative(int number) {
-        // the bug is that 0 will be reported as negative while it's not
-        return number <= 0;
+        return number < 0;
     }
 
 }
--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,34 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class NegativeNumberValidatorTest {
+
+    private NegativeNumberValidator SUT;
+
+    @Before
+    public void init() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1() {
+        boolean negative = SUT.isNegative(1);
+        Assert.assertFalse(negative);
+    }
+
+    @Test
+    public void test2() {
+        boolean negative = SUT.isNegative(0);
+        Assert.assertFalse(negative);
+    }
+
+    @Test
+    public void test3() {
+        boolean negative = SUT.isNegative(-1);
+        Assert.assertTrue(negative);
+    }
 
 }
